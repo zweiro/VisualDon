09a4f908b74481aaa625af2ee1bbebdcf8af433e
Robin Zweifel <robin.zweifel@gmail.com>
2021-04-30 10:51:25 +0200

    Exercice 2 du jour

--- 20210430/scraping/app.js
+++ 20210430/scraping/app.js
@@ -4,7 +4,7 @@ cl(temp1, 'div.thumbnail')
   .map(el => {
     let product = el.querySelector("a.title").textContent
     let price = el.querySelector("h4.price").textContent
-    let rating = el.querySelector("div.rating > p:not(pull-right)").dataset.rating
+    let rating = el.querySelector("div.ratings > p:not(.pull-right)").dataset.rating
 
     return {
         produit: product,
